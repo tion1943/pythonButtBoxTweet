678f3d4ec4483a353b6059f28ea54d542a6b65be
David Payne <tion1943@protonmail.com>
2017-03-05 17:18:57 -0500

    Added @user question to tweet

--- POC/balajiPOC.py
+++ POC/balajiPOC.py
@@ -1,13 +1,17 @@
 import random
 
 tweets = list()
-inputs = open('balaji.txt')
+inputs = open('../balaji.txt')
 for line in inputs:
     if (line != '\n') and not(line.startswith('#')):
         tweets.append(line.strip())
+tweetStr = str()        
 while(True):
-    tweetStr = random.choice(tweets)
-    if (len(tweetStr) <= 140):
-        break
-    
+    frontTweet = random.choice(tweets)
+    f = open('../user.txt').read()
+    names = f.strip().split(',')
+    name = random.choice(names)
+    backTweet = name + ' what do you think about this?'
+    tweetStr = frontTweet + '\n' + backTweet
+    if len(tweetStr) <= 140: break
 print tweetStr
